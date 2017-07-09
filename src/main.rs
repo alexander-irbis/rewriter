c4a5fc52bc84d973c08c550f0e5f2ca7b1c7df8b
Alexander Irbis <irbis.labs@gmail.com>
2017-07-09 12:19:56 +0000

    rewriter

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::env;
 use std::error::Error;
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::io;
+use std::io::Read;
 use std::io::Write;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::io::BufWriter;
 
 
 fn main() {
-    let mut owrp_ifile:Option<File> = None;
-    let mut owrp_ofile:Option<File> = None;
-
     let mut owrp_ifilename:Option<String> = None;
     let mut owrp_ofilename:Option<String> = None;
 
@@ -31,114 +31,34 @@ fn main() {
         i += 2;
     }
 
-    if let Some(filename) = owrp_ifilename {
-        match File::open(&filename) {
-            Err(why) => panic!("Couldn't open file \"{}\": {}", filename, why.description()),
-            Ok(file) => { owrp_ifile = Some(file) },
+
+    let input_buffer = owrp_ifilename
+        .map(|filename| {
+            File::open(&filename)
+                .map_err(|why| panic!("Couldn't open input file \"{}\": {}", filename, why.description()))
+                .unwrap()
+        })
+        .map(|file| Box::new(BufReader::new(file)) as Box<BufRead>)
+        .unwrap_or_else(|| Box::new(BufReader::new(io::stdin())) as Box<BufRead>);
+
+    let mut output_buffer = owrp_ofilename
+        .map(|filename| {
+            OpenOptions::new().create(true).append(true)
+                .open(&filename)
+                .map_err(|why| panic!("Couldn't open output file \"{}\": {}", filename, why.description()))
+                .unwrap()
+        })
+        .map(|file| Box::new(BufWriter::new(file)) as Box<Write>)
+        .unwrap_or_else(|| Box::new(BufWriter::new(io::stdout())) as Box<Write>);
+
+    for line in input_buffer.lines() {
+        let line: String = match line {
+            Err(why) => panic!("Couldn't read from file! Reason: {}", why),
+            Ok(line) => line
         };
-    }
 
-    if let Some(filename) = owrp_ofilename {
-        match File::create(&filename) {
-            Err(why) => panic!("Couldn't create {}: {}", filename, why.description()),
-            Ok(file) => { owrp_ofile = Some(file) },
-        }
-    }
-
-    match owrp_ifile {
-        Some(ifile) => {
-            let ifilebuf = BufReader::new(&ifile);
-
-            match owrp_ofile {
-                Some(mut ofile) => {
-                    for owrp_line in ifilebuf.lines() {
-                        let str_line = match owrp_line {
-                            Err(why) => panic!("Couldn't read from file! Reason: {}", why),
-                            Ok(line) => line
-                        };
-
-                        let line: String = str_line.to_string();
-
-                        match ofile.write(line.as_bytes()) {
-                            Err(e) => panic!("Can't write to file! Reason: {}", e),
-                            Ok(_) => {}
-                        }
-                        match ofile.write(b"\r\n") {
-                            Err(why) => panic!("Can't write to file! Reason: {}", why),
-                            Ok(_) => {}
-                        }
-                    }
-                },
-                None => {
-                    let stdout = io::stdout();
-                    let mut handle = stdout.lock();
-
-                    for line in ifilebuf.lines() {
-                        let str_line = match line {
-                            Err(why) => panic!("Can't read from file! Reason: {}", why),
-                            Ok(line) => line
-                        };
-
-                        let line: String = str_line.to_string();
-
-                        match handle.write(line.as_bytes()) {
-                            Err(why) => panic!("Can't write to STDOUT! Reason: {}", why),
-                            Ok(_) => {}
-                        };
-                        match handle.write(b"\r\n") {
-                            Err(why) => panic!("Can't write to STDOUT! Reason: {}", why),
-                            Ok(_) => {}
-                        }
-                    }
-                }
-            }
-        },
-        None => {
-            let stdin = io::stdin();
-
-            match owrp_ofile {
-                Some(mut ofile) => {
-                    for line in stdin.lock().lines() {
-                        let str_line = match line {
-                            Err(why) => panic!("Can't read from STDIN! Reason: {}", why),
-                            Ok(line) => line
-                        };
-
-                        let line: String = str_line.to_string();
-
-                        match ofile.write(line.as_bytes()) {
-                            Err(e) => panic!("Can't write to file! Reason: {}", e),
-                            Ok(_) => {}
-                        }
-                        match ofile.write(b"\r\n") {
-                            Err(why) => panic!("Can't write to file! Reason: {}", why),
-                            Ok(_) => {}
-                        }
-                    }
-                },
-                None => {
-                    let stdout = io::stdout();
-                    let mut handle = stdout.lock();
-
-                    for line in stdin.lock().lines() {
-                        let str_line = match line {
-                            Err(why) => panic!("Couldn't read from STDIN! Reason: {}", why),
-                            Ok(line) => line
-                        };
-
-                        let line: String = str_line.to_string();
-
-                        match handle.write(line.as_bytes()) {
-                            Err(why) => panic!("Couldn't write to STDOUT! Reason: {}", why),
-                            Ok(_) => {}
-                        }
-                        match handle.write(b"\r\n") {
-                            Err(why) => panic!("Can't write to STDOUT! Reason: {}", why),
-                            Ok(_) => {}
-                        }
-                    }
-                }
-            }
+        if let Err(e) = write!(output_buffer, "{}\r\n", line) {
+            panic!("Can't write to file! Reason: {}", e);
         }
     }
 }
